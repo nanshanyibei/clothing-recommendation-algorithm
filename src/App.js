a3b1c6390cf28bcd320945174c2da14b0349f33f
leijiajia <leijiajia@kuaishou.com>
2019-12-05 22:12:41 +0800

    添加部分UI涉及

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      contentDisplay: ['ajksdfjkasdfjksa','萨科技发数据开发开始']
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="search-area">
+          <div className="searchs-input">
+            <input className="sarch-input" value={this.state.inputValue} placeholder="请输入内容" onChange={e => {
+              this.setState({
+                inputValue: e.currentTarget.value
+              })
+            }}/>
+          </div>
+          <div className="searches-buttons">
+            <button className="search-button" onClick={()=>{
+              console.log("用户相关")
+            }}>用户相关</button>
+            <button className="search-button" onClick={()=>{
+              console.log("广告相关")
+            }}>广告相关</button>
+          </div>
+        </div>
+        <div className="content-display-area">
+          {this.state.contentDisplay.map(e => {
+            return <div key={e}>{e}</div>
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
